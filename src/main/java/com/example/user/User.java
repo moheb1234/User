2897cbb910c93b9452785a1612e05709b5ca285f
Mr. X <mohebmoallem1376@gmail.com>
2021-12-18 10:09:52 +0330

    first push

---
+++ src/main/java/com/example/user/User.java
@@ -0,0 +1,46 @@
+package com.example.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String firstname, lastname, email;
+
+    public User() {
+    }
+
+    public User(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/user/UserController.java
@@ -0,0 +1,26 @@
+package com.example.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping("/load")
+    public List<User> users() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/save")
+    User save(@RequestParam String firstname, @RequestParam String lastname, @RequestParam String email) {
+        User user = new User(firstname, lastname, email);
+        return repository.save(user);
+    }
+}
