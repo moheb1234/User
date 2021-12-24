18f0cd9e2e0079ae51432c640af845fb9e5d7e6e
Mr. X <mohebmoallem1376@gmail.com>
2021-12-24 18:08:35 +0330

    ""

--- src/main/java/com/example/user/JsonTools.java
+++ src/main/java/com/example/user/JsonTools.java
@@ -5,16 +5,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonTools {
     static ObjectMapper mapper = new ObjectMapper();
-    public static <T> T fromJson(String json , Class<T> tClass){
+
+    public static <T> T jsonToObject(String json, Class<T> tClass) {
         try {
-            return mapper.readValue(json,tClass);
+            return mapper.readValue(json, tClass);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public static<T>  String toJson(T t) {
+    public static <T> T[] jsonToArray(String json, Class<T[]> aClass) {
+        try {
+            return mapper.readValue( json , aClass);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static <T> String toJson(T t) {
         try {
             return mapper.writeValueAsString(t);
         } catch (JsonProcessingException e) {
--- src/test/java/com/example/user/UserControllerTest.java
+++ src/test/java/com/example/user/UserControllerTest.java
@@ -7,17 +7,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static com.example.user.JsonTools.toJson;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static com.example.user.JsonTools.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -33,41 +32,49 @@ class UserControllerTest {
     void save() throws Exception {
         User user = new User("moheb", "moallem", "@gmail6");
         when(repository.save(user)).thenReturn(user);
-        mockMvc.perform(post("/save")
-                        .param("firstname", user.getFirstname())
-                        .param("lastname", user.getLastname())
-                        .param("email", user.getEmail()))
+        MockHttpServletRequestBuilder request = post("/save").contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .param("firstname", user.getFirstname())
+                .param("lastname", user.getLastname())
+                .param("email", user.getEmail());
+        mockMvc.perform(request)
                 .andExpect(status().isOk());
     }
 
     @Test
     void users() throws Exception {
         User user = new User("1", "1", "@1");
+        User user2 = new User("2", "2", "@2");
         List<User> users = new ArrayList<>();
         users.add(user);
+        users.add(user2);
         when(repository.findAll()).thenReturn(users);
-        mockMvc.perform(get("/load"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].email", is(user.getEmail())))
-                .andExpect(jsonPath("$[0].lastname", is(user.getLastname())))
-                .andExpect(jsonPath("$[0].firstname", is(user.getFirstname())));
+        MvcResult response = mockMvc.perform(get("/load"))
+                .andExpect(status().isOk()).andReturn();
+        String json = response.getResponse().getContentAsString();
+        User[] userArray = jsonToArray(json, User[].class);
+        assert userArray != null;
+        for (int i = 0; i < users.size(); i++) {
+            assert users.get(i).toString().equals(userArray[i].toString());
+        }
     }
 
     @Test
     void getOneUser() throws Exception {
         User user = new User("1", "1", "1");
         when(repository.findById(user.getId())).thenReturn(Optional.of(user));
-        mockMvc.perform(get("/load/byId/0"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.email", is(user.getEmail())))
-                .andExpect(jsonPath("$.lastname", is(user.getLastname())))
-                .andExpect(jsonPath("$.firstname", is(user.getFirstname())));
+        MvcResult result = mockMvc.perform(get("/load/byId/0"))
+                .andExpect(status().isOk()).andReturn();
+        String body = result.getResponse().getContentAsString();
+        User resultUser = jsonToObject(body, User.class);
+        assert resultUser != null;
+        assert resultUser.toString().equals(user.toString());
     }
+
     @Test
     void updateUSer() throws Exception {
         User newUser = new User("2", "2", "@10");
-        when(repository.updateUser(newUser.getFirstname(),newUser.getFirstname(),newUser.getEmail(),0))
+        when(repository.updateUser(newUser.getFirstname(), newUser.getFirstname(), newUser.getEmail(), 0))
                 .thenReturn(1);
         String json = toJson(newUser);
         assert json != null;
@@ -79,11 +86,12 @@ class UserControllerTest {
     void findByEmail() throws Exception {
         User user = new User("1", "2", "@3");
         when(repository.findByEmail(user.getEmail())).thenReturn(user);
-        mockMvc.perform(get("/load/byEmail/@3"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.email", is(user.getEmail())))
-                .andExpect(jsonPath("$.lastname", is(user.getLastname())))
-                .andExpect(jsonPath("$.firstname", is(user.getFirstname())));
+        MvcResult result = mockMvc.perform(get("/load/byEmail/@3"))
+                .andExpect(status().isOk()).andReturn();
+        String json = result.getResponse().getContentAsString();
+        User foundedUser = jsonToObject(json,User.class);
+        assert foundedUser!=null;
+        assert foundedUser.toString().equals(user.toString());
     }
 
     @Test
@@ -91,14 +99,13 @@ class UserControllerTest {
         mockMvc.perform(delete("/delete/1")).andExpect(status().isOk());
     }
 
-
     @Test
     void saveV2() throws Exception {
         User user = new User("1,", "2", "@3");
         String json = toJson(user);
         when(repository.save(user)).thenReturn(user);
         assert json != null;
-        mockMvc.perform(post("/saveV2")
-                .content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+        MockHttpServletRequestBuilder request = post("/saveV2").content(json).contentType(MediaType.APPLICATION_JSON);
+         mockMvc.perform(request).andExpect(status().isOk());
     }
 }
