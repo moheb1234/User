475aa5f2be7830f7468789c17c83f09c1aca746f
Mr. X <mohebmoallem1376@gmail.com>
2022-01-01 00:57:22 +0330

    second reform

--- src/test/java/com/example/user/Test/UserControllerTest.java
+++ src/test/java/com/example/user/Test/UserControllerTest.java
@@ -4,7 +4,6 @@ package com.example.user.Test;
 import com.example.user.controller.UserController;
 import com.example.user.entities.User;
 import com.example.user.model.UserModel;
-import com.example.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -29,50 +28,38 @@ class UserControllerTest {
     MockMvc mockMvc;
 
     @MockBean
-    UserRepository repository;
-
-    @MockBean
     UserModel model;
-
 
     @Test
     void save() throws Exception {
-        User user = new User("moheb", "moallem", "@gmail6");
-        when(model.save(user.getFirstname(), user.getLastname(), user.getEmail())).thenReturn(user);
-        MockHttpServletRequestBuilder request = post("/save").contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .param("firstname", user.getFirstname())
-                .param("lastname", user.getLastname())
-                .param("email", user.getEmail());
-        MvcResult result = mockMvc.perform(request)
-                .andExpect(status().isOk()).andReturn();
+        User user = new User(1, "moheb", "moallem", "@gmail6");
+        when(model.save(1, user.getFirstname(), user.getLastname(), user.getEmail())).thenReturn(user);
+        MockHttpServletRequestBuilder request = post("/users").param("id", 1 + "").param("firstname", user.getFirstname()).param("lastname", user.getLastname()).param("email", user.getEmail());
+        MvcResult result = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
         String json = result.getResponse().getContentAsString();
         User resultUser = jsonToObject(json, User.class);
-        assert Objects.requireNonNull(resultUser).toString().equals(user.toString());
+        assert Objects.requireNonNull(resultUser).equals(user);
     }
 
     @Test
     void users() throws Exception {
-        User user = new User("1", "1", "@1");
-        User user2 = new User("2", "2", "@2");
+        User user = new User(1, "1", "1", "@1");
+        User user2 = new User(2, "2", "2", "@2");
         List<User> users = new ArrayList<>();
         users.add(user);
         users.add(user2);
         when(model.findAll()).thenReturn(users);
-        MvcResult response = mockMvc.perform(get("/load"))
-                .andExpect(status().isOk()).andReturn();
+        MvcResult response = mockMvc.perform(get("/users")).andExpect(status().isOk()).andReturn();
         String json = response.getResponse().getContentAsString();
         User[] userArray = jsonToArray(json, User[].class);
-        assert users.get(0).toString().equals(Objects.requireNonNull(userArray)[0].toString());
+        assert users.get(0).equals(Objects.requireNonNull(userArray)[0]);
     }
 
     @Test
     void getOneUser() throws Exception {
-        User user = new User("1", "1", "1");
-        user.setId("1");
+        User user = new User(1, "1", "1", "1");
         when(model.findById(user.getId())).thenReturn(user);
-        MvcResult result = mockMvc.perform(get("/load/byId/1"))
-                .andExpect(status().isOk()).andReturn();
+        MvcResult result = mockMvc.perform(get("/users/1")).andExpect(status().isOk()).andReturn();
         String body = result.getResponse().getContentAsString();
         User resultUser = jsonToObject(body, User.class);
         assert user.equals(resultUser);
@@ -80,23 +67,19 @@ class UserControllerTest {
 
     @Test
     void updateUSer() throws Exception {
-        User newUser = new User("2", "2", "@10");
-        newUser.setId("1");
-        when(model.update(newUser.getId(), newUser))
-                .thenReturn(newUser);
+        User newUser = new User(1, "2", "2", "@10");
+        when(model.update(newUser.getId(), newUser)).thenReturn(newUser);
         String json = toJson(newUser);
-        MvcResult response = mockMvc.perform(put("/update/1").content(json).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andReturn();
+        MvcResult response = mockMvc.perform(put("/users/1").content(Objects.requireNonNull(json)).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
         User resultUser = jsonToObject(response.getResponse().getContentAsString(), User.class);
         assert Objects.equals(resultUser, newUser);
     }
 
     @Test
     void findByEmail() throws Exception {
-        User user = new User("1", "2", "@3");
+        User user = new User(1, "1", "2", "@3");
         when(model.findByEmail(user.getEmail())).thenReturn(user);
-        MvcResult result = mockMvc.perform(get("/load/byEmail/@3"))
-                .andExpect(status().isOk()).andReturn();
+        MvcResult result = mockMvc.perform(get("/users/find-by-email/@3")).andExpect(status().isOk()).andReturn();
         String json = result.getResponse().getContentAsString();
         User foundedUser = jsonToObject(json, User.class);
         assert Objects.requireNonNull(foundedUser).toString().equals(user.toString());
@@ -104,16 +87,10 @@ class UserControllerTest {
 
     @Test
     void deleteUser() throws Exception {
-        User user = new User("3", "4", "alimoheb@gmail.com");
-        user.setId("1");
-            when(model.delete("1")).thenReturn(1);
-        try {
-            MvcResult response = mockMvc.perform(delete("/delete/1")).andExpect(status().isOk()).andReturn();
-            String body = response.getResponse().getContentAsString();
-            assert body.equals("1");
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+        User user = new User(1, "3", "4", "alimoheb@gmail.com");
+        when(model.delete(user.getId())).thenReturn(1);
+        MvcResult response = mockMvc.perform(delete("/users/1")).andExpect(status().isOk()).andReturn();
+        String body = response.getResponse().getContentAsString();
+        assert body.equals("1");
     }
 }
